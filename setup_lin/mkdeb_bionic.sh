c69818fc0e14aa27f5c4239842791afb72c60ce4
Tobias WEBER <tweber@ill.fr>
2020-10-13 10:24:29 +0200

    make: added external tools

--- setup_lin/mkdeb_bionic.sh
+++ setup_lin/mkdeb_bionic.sh
@@ -59,6 +59,11 @@ popd
 cp -v bin/takin_cif2xml		${INSTDIR}/usr/local/bin
 cp -v bin/takin_findsg		${INSTDIR}/usr/local/bin
 cp -v bin/takin_pol		${INSTDIR}/usr/local/bin
+cp -v bin/takin_structfact      ${INSTDIR}/usr/local/bin
+cp -v bin/takin_magstructfact   ${INSTDIR}/usr/local/bin
+cp -v bin/takin_scanbrowser     ${INSTDIR}/usr/local/bin
+cp -v bin/takin_magsgbrowser    ${INSTDIR}/usr/local/bin
+cp -v bin/takin_moldyn          ${INSTDIR}/usr/local/bin
 
 
 # permissions
--- setup_lin/mkdeb_focal.sh
+++ setup_lin/mkdeb_focal.sh
@@ -45,7 +45,7 @@ cp -v COPYING			${INSTDIR}/usr/local/share/takin
 cp -v LICENSES			${INSTDIR}/usr/local/share/takin
 cp -v LITERATURE		${INSTDIR}/usr/local/share/takin
 cp -v AUTHORS			${INSTDIR}/usr/local/share/takin
-cp -v setup_lin/takin.desktop		${INSTDIR}/usr/share/applications
+cp -v setup_lin/takin.desktop	${INSTDIR}/usr/share/applications
 cp -v /usr/local/lib/libMinuit2.so ${INSTDIR}/usr/local/lib
 
 pushd ${INSTDIR}/usr/local/lib
@@ -59,6 +59,11 @@ popd
 cp -v bin/takin_cif2xml		${INSTDIR}/usr/local/bin
 cp -v bin/takin_findsg		${INSTDIR}/usr/local/bin
 cp -v bin/takin_pol		${INSTDIR}/usr/local/bin
+cp -v bin/takin_structfact	${INSTDIR}/usr/local/bin
+cp -v bin/takin_magstructfact	${INSTDIR}/usr/local/bin
+cp -v bin/takin_scanbrowser	${INSTDIR}/usr/local/bin
+cp -v bin/takin_magsgbrowser	${INSTDIR}/usr/local/bin
+cp -v bin/takin_moldyn		${INSTDIR}/usr/local/bin
 
 
 # permissions
