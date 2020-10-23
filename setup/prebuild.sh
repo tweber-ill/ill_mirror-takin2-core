65b3877fb8aa9dc0c89f38067c8adff02193e7d7
Tobias WEBER <tweber@ill.fr>
2020-10-23 09:57:18 +0200

    doc: added alternate help collection generator call

--- setup/prebuild.sh
+++ setup/prebuild.sh
@@ -7,11 +7,18 @@
 
 if [[ $# -ge 2 ]]; then
 	COLGEN="$(dirname $2)/qhelpgenerator"
+	COLGEN_ALT="$(dirname $2)/qcollectiongenerator"
+
 	TAKINROOT="$1"
 else
 	COLGEN="$(which qhelpgenerator-qt5)"
 	if [ $? -ne 0 ]; then
 		COLGEN="$(which qhelpgenerator)"
+	fi
+
+	COLGEN_ALT="$(which qcollectiongenerator-qt5)"
+	if [ $? -ne 0 ]; then
+		COLGEN_ALT="$(which qcollectiongenerator)"
 	fi
 
 	TAKINROOT=.
@@ -26,6 +33,12 @@ echo -e "-----------------------------------------------------------------------
 echo -e "building docs using collection generator: ${COLGEN}..."
 
 ${COLGEN} ${TAKINROOT}/doc/takin.qhcp -o ${TAKINROOT}/doc/takin.qhc
+
+# try alternate help collection generator
+if [ ! -f ${TAKINROOT}/doc/takin.qhc ]; then
+	${COLGEN_ALT} ${TAKINROOT}/doc/takin.qhcp -o ${TAKINROOT}/doc/takin.qhc
+fi
+
 cp -v ${TAKINROOT}/doc/takin.qhc ${TAKINROOT}/res/doc/
 cp -v ${TAKINROOT}/doc/takin.qch ${TAKINROOT}/res/doc/
 
