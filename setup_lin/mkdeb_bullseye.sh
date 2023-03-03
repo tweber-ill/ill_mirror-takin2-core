19f2cea6cfbf93c024f870f28108c07612128c2d
Tobias WEBER <tweber@ill.fr>
2023-03-03 08:23:43 +0100

    preparing for 2.5.1 release

--- setup_lin/mkdeb_bullseye.sh
+++ setup_lin/mkdeb_bullseye.sh
@@ -47,9 +47,9 @@ mkdir -p ${INSTDIR}/DEBIAN
 
 
 # control file
-echo -e "Package: takin\nVersion: 2.5.0" > ${INSTDIR}/DEBIAN/control
+echo -e "Package: takin\nVersion: 2.5.1" > ${INSTDIR}/DEBIAN/control
 echo -e "Description: inelastic neutron scattering software" >> ${INSTDIR}/DEBIAN/control
-echo -e "Maintainer: Tobias Weber <tobias.weber@ill.fr>" >> ${INSTDIR}/DEBIAN/control
+echo -e "Maintainer: Tobias Weber <tweber@ill.fr>" >> ${INSTDIR}/DEBIAN/control
 echo -e "Architecture: $(dpkg --print-architecture)" >> ${INSTDIR}/DEBIAN/control
 echo -e "Section: science\nPriority: optional" >> ${INSTDIR}/DEBIAN/control
 echo -e "Depends:" \
